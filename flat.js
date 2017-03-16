9c6cd3c540d19417bdde98b74e25edff8631bf69
Erik Pukinskis <erik@snowedin.net>
2017-03-15 19:37:58 -0700

    add nesting and a little color to renderer

--- flat.js
+++ flat.js
@@ -3,50 +3,70 @@ var library = require("module-library")(require)
 library.using(
   ["web-host", "web-element"],
   function(host, element) {
+    var leftParen = element(".call-symbol", "(")
+    var rightParen = element(".call-symbol", ")")
+    var leftCurly = element(".scope-symbol", "{")
+    var rightCurly = element(".scope-symbol", "}")
+    var leftBracket = element(".array-symbol", "[")
+    var rightBracket = element(".array-symbol", "]")
+
     var page  = element(".lines", [
-      element("function buildAHouse("),
-      element(".depth-1", "issueBond,"),
-      element(".depth-1", "webHost,"),
-      element(".depth-1", "library,"),
-      element(".depth-1", "renderBond ){"),
-      element(".break"),
-      element(".depth-2", "var buildPanel ="),
-      element(".depth-3", "issueBond(["),
-      element(".depth-4", "cut studs to length,"),
-      element(".depth-4", "cut track to length,"),
-      element(".depth-4", "crimp,"),
-      element(".depth-4", "add sheathing,"),
-      element(".depth-4", "flipsulate,"),
-      element(".depth-4", "add sheathing ])"),
-      element(".break"),
-      element(".depth-2", "issueBond.expense("),
-      element(".depth-3", "buildPanel,"),
-      element(".depth-3", "labor,"),
-      element(".depth-3", "$100)"),
-      element(".break"),
-      element(".depth-2", "issueBond.expense("),
-      element(".depth-3", "buildPanel,"),
-      element(".depth-3", "steel studs,"),
-      element(".depth-3", "$20 )"),
-      element(".break"),
-      element(".depth-2", "issueBond.expense("),
-      element(".depth-3", "buildPanel,"),
-      element(".depth-3", "plywood,"),
-      element(".depth-3", "$10 )"),
-      element(".break"),
-      element(".depth-2", "webHost.hostModule("),
-      element(".depth-3", "library,"),
-      element(".depth-3", "render-bond,"),
-      element(".depth-3", "buildPanel )"),
-      element(".break"),
-      element(".depth-2", "return buildPanel }"),
+      element(element(".function-symbol", "function"), "buildAHouse", leftParen),
+      element(".depth-1.argument-name", "issueBond,"),
+      element(".depth-1.argument-name", "webHost,"),
+      element(".depth-1.argument-name", "library,"),
+      element(".depth-1.argument-name", "renderBond", rightParen, leftCurly),
+      element(".scope.depth-1", [
+        element(element(".variable-symbol", "var"), "buildPanel ="),
+        element(".depth-1",
+          "issueBond",
+          leftParen,
+          leftBracket
+        ),
+
+        element(".array", [
+          element(".depth-2.array-item", "cut studs to length,"),
+          element(".depth-2.array-item", "cut track to length,"),
+          element(".depth-2.array-item", "crimp,"),
+          element(".depth-2.array-item", "add sheathing,"),
+          element(".depth-2.array-item", "flipsulate,"),
+          element(".depth-2.array-item",
+            "add sheathing",
+            rightBracket,
+            rightParen
+          ),
+        ]),
+
+        element(".break"),
+        element("issueBond.expense", leftParen),
+        element(".depth-1", "buildPanel,"),
+        element(".depth-1", "labor,"),
+        element(".depth-1", "$100", rightParen),
+        element(".break"),
+        element("issueBond.expense", leftParen),
+        element(".depth-1", "buildPanel,"),
+        element(".depth-1", "steel studs,"),
+        element(".depth-1", "$20", rightParen),
+        element(".break"),
+        element("issueBond.expense", leftParen),
+        element(".depth-1", "buildPanel,"),
+        element(".depth-1", "plywood,"),
+        element(".depth-1", "$10", rightParen),
+        element(".break"),
+        element("webHost.hostModule", leftParen),
+        element(".depth-1", "library,"),
+        element(".depth-1", "render-bond,"),
+        element(".depth-1", "buildPanel", rightParen),
+        element(".break"),
+        element("return buildPanel", leftCurly),
+      ])
     ])
 
     var stylesheet = element.stylesheet([
       element.style(".lines", {
         "font-family": "sans-serif",
-        "font-size": "13pt",
-        "color": "#333",
+        "font-size": "15pt",
+        "color": "#557",
         "line-height": "1.2em",
       }),
       element.style(".depth-1", {
@@ -66,6 +86,55 @@ library.using(
         "width": "1.5em",
         "background": "#fff"
       }),
+      element.style(".array-symbol", {
+        "color": "#a9a9ff",
+        "display": "inline-block",
+        "padding-left": "0.5em",
+        "font-weight": "bold",
+      }),
+      element.style(".array-item", {
+        "border-left": "3px solid #a9a9ff",
+        "padding-left": "0.5em",
+      }),
+
+      element.style(".scope-symbol", {
+        "color": "#f5df2f",
+        "display": "inline-block",
+        "padding-left": "0.5em",
+        "font-weight": "bold",
+      }),
+
+      element.style(".scope", {
+        "border-left": "3px solid #f5df2f",
+        "padding-left": "0.5em",
+      }),
+
+      element.style(".call-symbol", {
+        "color": "#bec9d6",
+        "display": "inline-block",
+        "padding-left": "0.5em",
+        "font-weight": "bold",
+      }),
+
+      element.style(".function-symbol", {
+        "color": "#bec9d6",
+        "display": "inline-block",
+        "padding-right": "0.5em",
+        "font-weight": "bold",
+      }),
+
+      element.style(".argument-name", {
+        "color": "#bec9d6",
+      }),
+
+      element.style(".variable-symbol", {
+        "color": "black",
+        "display": "inline-block",
+        "padding-right": "0.5em",
+        "font-weight": "bold",
+      }),
+
+
     ])
 
     host.onVoxel(function(voxel) {
